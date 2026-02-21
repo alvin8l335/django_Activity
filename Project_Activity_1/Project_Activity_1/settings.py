10d50bf89f1582b0e2c302ce6af751568ebada56
Tinoy <alvintinoy125@gmail.com>
2026-02-21 19:06:23 +0800

    Connected project to PostgreSQL and updated settings

--- Project_Activity_1/Project_Activity_1/settings.py
+++ Project_Activity_1/Project_Activity_1/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Project_Activity_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'taYcUnvGEJWpYvEEPDdMSjanWOEMYzos',
+        'HOST': 'yamanote.proxy.rlwy.net',
+        'PORT': '25389',
     }
 }
 
